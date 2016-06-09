6f3c101aabb2bbdefaf5f7acd5eab735a7155a4f
ktw016 <ktw016@gmail.com>
2016-06-09 12:59:29 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-for (var i=1; i <= 100; i++)
-{
-    if (i % 15 == 0)
-        console.log("FizzBuzz");
-    else if (i % 5 == 0)
-        console.log("Buzz");
-    else if (i % 3 == 0)
-        console.log("Fizz");    
-    else
-        console.log(i);
-}
+
+
+	for (var i=1; i <= 100; i++) {
+    	if (i % 3 === 0 && i % 5 === 0) {
+        	$('body').append("FizzBuzz" + "<br>");
+    	}
+    	else if (i % 5 === 0) {
+        	$('body').append("Buzz" + "<br>");
+    	}
+    	else if (i % 3 === 0) {
+        	$('body').append("Fizz" + "<br>");   
+    	}
+    	else {
+        	$('body').append(i + "<br>");
+    	}
+	};
+
+
