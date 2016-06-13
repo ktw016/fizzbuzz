17db49742eebf3e10626c571dc5622a2eb37af2c
ktw016 <ktw016@gmail.com>
2016-06-13 12:53:08 -0700

    refactor

--- js/app.js
+++ js/app.js
@@ -1,18 +1,41 @@
+$(document).ready(function() {
+	function userInput() {
+		var inputNumber = +parseInt(prompt("Enter an integer 1-100"));
+		if (inputNumber === 0) {
+			console.log('inputNumber')
+			return;
+		}
+		else if (isNaN(inputNumber)) {
+			alert ("Not a number!");
+			userInput;
+		}
+		else {
+			results(inputNumber);
+		}
+	}
 
-
-	for (var i=1; i <= 100; i++) {
-    	if (i % 3 === 0 && i % 5 === 0) {
-        	$('body').append("FizzBuzz" + "<br />");
-    	}
-    	else if (i % 5 === 0) {
-        	$('body').append("Buzz" + "<br />");
-    	}
-    	else if (i % 3 === 0) {
-        	$('body').append("Fizz" + "<br />");   
-    	}
-    	else {
-        	$('body').append(i + "<br />");
-    	}
+	
+	function results(inputNumber) {
+		for (var i=1; i <= inputNumber; i++) {
+    		if (i % 3 === 0 && i % 5 === 0) {
+        		$('#root').append("FizzBuzz" + "<br />");
+    		}
+    		else if (i % 5 === 0) {
+        		$('#root').append("Buzz" + "<br />");
+    		}
+    		else if (i % 3 === 0) {
+        		$('#root').append("Fizz" + "<br />");   
+    		}
+    		else if (i % 1 != 0) {
+				alert ("No decimals!");
+				userInput;
+			}
+    		else {
+        		$('#root').append(i + "<br />");
+    		}
+		};
 	};
-
+	userInput();
+});
+	
 
